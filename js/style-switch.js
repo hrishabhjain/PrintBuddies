dade18801cdd9be08603ce8dc9584fdae7eac8cf
Rishabh Jain <rishabhjain5858@gmail.com>
2015-03-21 15:44:55 +0530

    Feedback Form

--- js/style-switch.js
+++ js/style-switch.js
@@ -1,8 +1,8 @@
 
 
- 
- 
- 
+
+
+
 
 
 
@@ -10,56 +10,6 @@
 
 
 jQuery(document).ready(function($){
-$(".blue-red").click(function(){$("#colorstyle").attr("href","css/skin/blue-red.css");
-return false;
-});
-$(".midnight-blue").click(function(){$("#colorstyle").attr("href","css/skin/midnight-blue.css");
-return false;
-});
-$(".mono-red").click(function(){$("#colorstyle").attr("href","css/skin/mono-red.css");
-return false;
-});
-$(".pinegreen-purple").click(function(){$("#colorstyle").attr("href","css/skin/pinegreen-purple.css");
-return false;
-});
-
-$(".darkmagenta-rosybrown").click(function(){$("#colorstyle").attr("href","css/skin/darkmagenta-rosybrown.css");
-return false;
-});
-
-$(".darkorchid-seagreen").click(function(){$("#colorstyle").attr("href","css/skin/darkorchid-seagreen.css");
-return false;
-});
-
-$(".steel-blue").click(function(){$("#colorstyle").attr("href","css/skin/steel-blue.css");
-return false;
-});
-
-$(".cadetblue-violetred").click(function(){$("#colorstyle").attr("href","css/skin/cadetblue-violetred.css");
-return false;
-});
-
-$(".mediumpurple-seagreen").click(function(){$("#colorstyle").attr("href","css/skin/mediumpurple-seagreen.css");
-return false;
-});
-
-$(".steelblue-leafgreen").click(function(){$("#colorstyle").attr("href","css/skin/steelblue-leafgreen.css");
-return false;
-});
-
-$(".orange-steelblue").click(function(){$("#colorstyle").attr("href","css/skin/orange-steelblue.css");
-return false;
-});
-
-$(".gray").click(function(){$("#colorstyle").attr("href","css/skin/gray.css");
-return false;
-});
-
-
-$("#reset").click(function(){$("#colorstyle").attr("href","css/skin/blue-red.css");
-return false;
-});
-
 $("#fullwidth").click(function(){$(".container").css("width:100%;");
 return false;
 });
